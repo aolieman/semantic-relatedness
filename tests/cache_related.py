1755de772780ffe079d3b9ca40bf5e2e963687a9
Alex Olieman <alex@olieman.net>
2014-05-27 16:38:54 +0200

    Included get_flow_map calls, because get_category_map only dereferences categories multiple levels out. The desired behavior is to dereference all neighboring resources.

--- tests/cache_related.py
+++ tests/cache_related.py
@@ -57,6 +57,34 @@ def get_category_map(topic_slug_list, language_code, port=8182):
     except Exception as e:
         print "Server error:", e
         return {"n_found_cats": 0}
+
+def get_flow_map(topic_slug_list, language_code, port=8182):
+    # Build the Gremlin script string
+    if language_code == u'en':
+        escaped_slugs = [u'"{0}"'.format(encode_non_ascii(slug)) for slug in topic_slug_list
+                         if slug is not None and is_possible_wiki_slug(slug)]
+    else:
+        escaped_slugs = [u'"{0}"'.format(slug) for slug in topic_slug_list
+                         if slug is not None and is_possible_wiki_slug(slug)]
+    script_value = u"getFlowMap([{0}], '{1}')".format(u", ".join(escaped_slugs), language_code)
+
+    url = base_url.format(port=port, path="graphs/dbp-sail/tp/gremlin")
+    payload = {'script': script_value, 'load': "[v0_categories]"}
+
+    print url, json.dumps(payload)
+
+    resp = requests.post(url, data=payload)
+
+    try:
+        resp_dict = resp.json()
+        if resp.status_code is not 200:
+            print resp_dict
+            return {"n_related_topics": 0}
+        resp_dict['n_related_topics'] = len(resp_dict["results"][0])
+        return resp_dict
+    except Exception as e:
+        print "Server error:", e
+        return {"n_related_topics": 0}
         
 def cache_related_resources(dir_path):
     ann_file_paths = [os.path.join(dir_path, fn) for fn 
@@ -71,3 +99,5 @@ def cache_related_resources(dir_path):
                 slug_set.add(unicode(cand[u'uri']))
         cat_map = get_category_map(list(slug_set), 'en')
         print "N found cats:", cat_map['n_found_cats'], "\n"
+        flow_map = get_flow_map(list(slug_set), 'en')
+        print "N related resources:", flow_map['n_related_topics'], "\n"
