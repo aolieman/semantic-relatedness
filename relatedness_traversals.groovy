2ee843cabbce24cf79c0557744a482a2b9e53825
Alex Olieman <alex@olieman.net>
2014-05-29 00:57:41 +0200

    New normalized category-flow traversal

---
+++ relatedness_traversals.groovy
@@ -0,0 +1,50 @@
+// Define categories to ignore (e.g. supernodes)
+def ign = [g.uri('dbp-nl:Categorie:Alles'), g.uri('dbp-nl:Categorie:Lijsten'),
+           g.uri('dbp-nl:Categorie:Wikipedia'), g.uri('dbp:Category:Container_categories'),
+           g.uri('dbp:Category:Fundamental_categories'), g.uri('dbp:Category:Main_topic_classifications')]
+
+def combine( Map... m ) {
+  m.collectMany { it.entrySet() }.inject( [:] ) { result, e ->
+    result << [ (e.key):e.value + ( result[ e.key ] ?: 0 ) ]
+  }
+}
+
+def getCatFlowMap( topic_list, lang_code ){
+    total_flow = [:]
+
+    for (topic_slug in topic_list) {
+        // Process one topic URI
+        flow = [:]
+        if (lang_code == "nl") {
+            v = g.v("http://nl.dbpedia.org/resource/$topic_slug")
+        } else {
+            v = g.v("http://dbpedia.org/resource/$topic_slug")
+        }
+
+        // Count flow (Sibling, Narrower, Broader, and Cousin topics)
+
+        v.out('dcterms:subject').dedup().filter{!ign.contains(it.id)}.as('mothers')
+         .in('dcterms:subject').groupCount(flow){it.id}{it.b+1.0}  // Sibling w1.0
+         .back('mothers')
+         .both('skos:broader').filter{!ign.contains(it.id)}.in('dcterms:subject')
+         .groupCount(flow){it.id}{it.b+0.5}.iterate()                      // Narrower & Broader w0.5
+
+         // Normalize flow (as fraction)
+         flow.remove(v.id)
+         flow.each{ it -> flow[it.key] = it.value / flow.values().sum() }
+
+         // Update total_flow to reflect average flow
+         if (topic_list.size() > 1) {
+             flow.each{ it -> flow[it.key] = it.value / topic_list.size() }
+         }
+         total_flow = combine(total_flow, flow)
+
+    }
+
+    // Sort by flow count
+    max_topics = 50
+    if (total_flow.size() < max_topics){max_topics = total_flow.size()}
+    flow_sorted = total_flow.sort{ a,b -> b.value <=> a.value }[0..max_topics-1]
+
+    return flow_sorted
+}
