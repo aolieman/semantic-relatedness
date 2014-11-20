305a857c01cd1729aa41800531560e95e3bd4e6a
Alex Olieman <alex@olieman.net>
2014-11-20 20:54:32 +0100

    Awk functions for N-Triples inspection(e.g. datatype schema inference)

--- rdf_loading/awk_nt_inspection.sh
+++ rdf_loading/awk_nt_inspection.sh
@@ -7,7 +7,7 @@ alias rdf-datatypes="awk -F'\x5E' '/\"\^\^</ { print substr(\$3, 2, length(\$3)-
 
 # Custom N-Triples aliases
 alias rdf-pred-dt="awk -F'> [<\"]' '/\"\^\^</ { match(\$2, /(#|y\/)([a-zA-z]+)/, pred); match(\$3, /#([a-zA-Z]+)>/, obj); print pred[2], obj[1] }' | uniq"
+alias rdf-schema-inference="awk -F'> [<\"]' '/^\s*[^#]/ { if (split(\$3, obj, /\"@/) > 1) range = \"@\" substr(obj[2], 0, length(obj[2])-2); else if (split(\$3, obj, /\"\^\^</) > 1) range = substr(obj[2], 0, length(obj[2])-3); else if (substr(\$3, length(\$3)-2, length(\$3)) == \"> .\") range = \"uri\"; else range = \"string\"; print substr(\$2, 1, length(\$2)), range }' | uniq"
 
-# TODO: find pred with plain literals /\" \./
-# TODO: find pred with lang-code literals /\"@/
-# TODO: find pred with URI objects
+# Find duplicate predicates in the inferred schema
+function duplicate-predicates() { awk '{print $1}' $1 | uniq -d | grep -F -f - $1; }
