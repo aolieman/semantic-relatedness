0ca96a723e51e378fc9e6d5f02dced4fc485a01b
Alex Olieman <alex@olieman.net>
2016-12-01 20:37:25 +0100

    Updated downloading script

--- rdf_loading/download_dumps.sh
+++ rdf_loading/download_dumps.sh
@@ -2,7 +2,7 @@
 # Download DBpedia dumps to the given directory
 
 USGSTR="Usage: download_dumps.sh <download_root_dir> <language_code (e.g. 'en')>"
-DBP_VERSION=2014
+DBP_VERSION="2015-10"
 echo "Downloading dumps for DBpedia v$DBP_VERSION. Edit the script source to change this version."
 
 if [ -n "$1" ] && [ -n "$2" ]; then
@@ -13,12 +13,12 @@ else
   exit 1
 fi
 
-FNAMES=( "article_categories_$lc.nt.bz2" "category_labels_$lc.nt.bz2" "disambiguations_$lc.nt.bz2" 
-         "geo_coordinates_$lc.nt.bz2" "instance_types_$lc.nt.bz2" 
-         "interlanguage_links_$lc.nt.bz2" "labels_$lc.nt.bz2" "page_links_$lc.nt.bz2" "redirects_$lc.nt.bz2" 
-         "short_abstracts_$lc.nt.bz2" "skos_categories_$lc.nt.bz2" "mappingbased_properties_$lc.nt.bz2")
+FNAMES=( "article_categories_$lc.ttl.bz2" "category_labels_$lc.ttl.bz2" "disambiguations_$lc.ttl.bz2" 
+         "long_abstracts_$lc.ttl.bz2" "instance_types_$lc.ttl.bz2" 
+         "interlanguage_links_$lc.ttl.bz2" "labels_$lc.ttl.bz2" "redirects_$lc.ttl.bz2" 
+         "short_abstracts_$lc.ttl.bz2" "skos_categories_$lc.ttl.bz2" "mappingbased_properties_$lc.ttl.bz2")
 if [ $lc -eq "en" ]; then
-    FNAMES+=( "instance_types_heuristic_$lc.nt.bz2" )
+    FNAMES+=( "instance_types_heuristic_$lc.ttl.bz2" )
 fi
          
 for fn in "${FNAMES[@]}"
@@ -27,7 +27,7 @@ do
     if [ -s "$fpath" ]; then
         echo "$fpath already exists"
     else
-        url="http://data.dws.informatik.uni-mannheim.de/dbpedia/$DBP_VERSION/$lc/$fn"
+        url="http://downloads.dbpedia.org/$DBP_VERSION/core-i18n/$lc/$fn"
         echo "Downloading $url..."
         wget -P "$target_dir/$lc/" "$url"
     fi
